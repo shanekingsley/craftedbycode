4e7278edea1dada43421409f75fdcc9c8d0616a4
Shane Kingsley <shane.kingsley87@gmail.com>
2025-05-27 21:27:27 +0100

    Setting up actions

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Crafted by Code Blog';
+export const SITE_DESCRIPTION = 'Thoughts on software, tools, and clean design by Shane Kingsley.';
